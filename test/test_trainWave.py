16daf50950d59907d9f1adddb9231441b19156d1
daniel <daniel@daniel-Z97P-D3>
2016-09-26 17:04:25 +0200

    .

--- test/test_trainWave.py
+++ test/test_trainWave.py
@@ -6,13 +6,15 @@ Created on Sep 26, 2016
 import unittest
 from loadWave import loadWave
 from trainWave import trainWave
-
+import numpy as np
 
 class test_trainWave(unittest.TestCase):
 
 
     def test(self):
-        waveform = loadWave('/home/daniel/daniel/sin.wav')
+        #waveform = loadWave('/home/daniel/daniel/sin.wav')
+        x_data = np.arange(100).astype(np.float32)/100.0
+        waveform = x_data * 0.1 + 0.3
         trainWave(waveform)
     
 
--- trainWave.py
+++ trainWave.py
@@ -3,6 +3,8 @@ Created on Sep 26, 2016
 
 @author: daniel
 '''
+import tensorflow as tf
+import numpy as np
 
 def trainWave(waveform):
     ''' Trains wave model.
@@ -11,5 +13,23 @@ def trainWave(waveform):
         np.array(int) Waveform array of amplitudes
     '''
     
-    raise Exception('Not implemented')     
+    W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+    b = tf.Variable(tf.zeros([1]))
     
+    xData = np.arange(100).astype(np.float32)/100.0
+    
+    y = W * xData + b
+    
+    loss = tf.reduce_mean(tf.square(waveform.astype(np.float32)-y))
+    
+    train = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+    
+    sess = tf.Session()
+    sess.run(tf.initialize_all_variables())
+    
+    for i in range(200):
+        sess.run(train)
+        print(sess.run(loss))
+        #print(sess.run(W),sess.run(b))
+        
+    raise Exception('Not implemented')     
